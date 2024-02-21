d28ab588caa47b92efc7926d3769663ac04b4cbf
ArthurRenato <artl1@aluno.ifal.edu.br>
2024-02-21 17:52:50 -0300

    Com 2 telas

--- src/navegacao/StackNavigator.js
+++ src/navegacao/StackNavigator.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../telas/HomeScreen";
+import LoginScreen from "../telas/LoginScreen";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
